598d3f4cfe1fee095cce22fc8e624ef890fae055
Phuong Le <PhuongLe@MBP-cua-Mr.net.fpt>
2018-09-30 16:05:26 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine','hbs');
+app.use(express.static(__dirname +'/public'));
+
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear();
+})
+
+app.get('/',(req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    wellcomeMessage: 'Wellcome to my Website'
+
+  })
+})
+
+app.get('/about',(req,res) => {
+  res.render('about.hbs',{
+    pageTitle: 'About Page'
+
+  });
+})
+app.listen(3000);
